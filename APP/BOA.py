4f9ee20d1cfb2f36f522ac32ec69d7c3b5ea7f2a
AKIO\秋生 <akio.kistokazaki@hope.edu>
2017-04-27 13:54:46 +0900

    finished coding

--- APP/BOA.py
+++ APP/BOA.py
@@ -33,11 +33,11 @@ fdistdict = {}
 
 # create Listboxs
 # now a Text on the left with font 11
-lb = Text(root,state=DISABLED,font=("consolas",11),width=50)##Listbox(root,font=("consolas",11)) #Times
+lb = Text(root,state=DISABLED,font=("consolas",11),width=50,height=20)##Listbox(root,font=("consolas",11)) #Times
 # list box for source files on top right
-lbs = Listbox(root)
+lbs = Listbox(root,height=8)
 # list box for dh files on bottum right
-lbdh = Listbox(root, selectmode=MULTIPLE)
+lbdh = Listbox(root, selectmode=MULTIPLE,height=8)
 
 # create Scrollbar vertical and horizontal for each list box
 sb1 = Scrollbar(root, orient = 'v', command = lb.yview)
@@ -59,57 +59,17 @@ lbs.configure(exportselection=False)
 buffer = StringVar(root)
 buffer.set("20")
 n_spin = Spinbox(root, from_=1, to=999, textvariable=buffer,width=3)
-
 # set serarch box
 search_buffer = StringVar()
 e = Entry(root, textvariable = search_buffer)
-
-# set the size of things
-lb_size = 180
-# 40% each, total 80 %
-side_ld_size =  int(lb_size/2.5)
-# 5% each 20% total
-reg_size = int(lb_size/20)
-lb_size = lb_size-reg_size
-left = 30
-right = 10
-pad = 3
-search_width = int(left/1.5)
-spin_width = int(left/6)
 # create labels
 lbs_label = Label(root, text="Sources")
 lbdh_label = Label(root, text=DH_FILE_CALLED+" Files")
-
 # create a check box for two options
 nn = BooleanVar()
 nn.set(True)
 naming = BooleanVar()
 naming.set(False)
-
-# set the grid
-# left side
-lb.grid(row = 0, column = 0, sticky = 'nsew',  padx = pad, pady = pad,rowspan=lb_size,columnspan= left) # ipadx = 200, ipady = 140,
-sb1.grid(row = 0, column = left, sticky = 'ns',rowspan=lb_size)
-#no need of the x scroll bar since we are using Text now
-#sb2 = Scrollbar(root, orient = 'h', command = lb.xview)
-#lb.configure(xscrollcommand = sb2.set)
-#sb2.grid(row = lb_size, column = 0, sticky = 'ew',columnspan= left)
-
-e.grid(row = lb_size+1, column = 0,columnspan = search_width, sticky = 'ew',  padx = 10, pady = 7,rowspan=reg_size)
-n_spin.grid(row = lb_size+1, column = search_width+spin_width,columnspan = spin_width, sticky = 'ew',  padx = 2, pady = 2,rowspan=reg_size)
-
-lbs_label.grid(row = 0, column = left+1, pady = pad,padx=pad,sticky = 'ew',rowspan=reg_size,columnspan=right)
-lbs.grid(row =  reg_size+1,column = left+1, pady = 0,padx=0,sticky = 'ns',rowspan=side_ld_size-reg_size,columnspan=right)#ipady = 40
-ssb1.grid(row =  reg_size+1,column = right +left, sticky = 'ns',columnspan=right,rowspan=side_ld_size-reg_size)
-ssb2.grid(row =  side_ld_size+1, column = left+1, sticky = 'ew',rowspan=reg_size,padx=pad,columnspan=right)
-Checkbutton(text = 'Source with "n:n"', variable = nn).grid(row = side_ld_size+reg_size+1, column = left+1, pady = 0,padx=8,sticky = 'w',rowspan=reg_size,columnspan=right)
-lbdh_label.grid(row = int(reg_size *2+side_ld_size+1), column = left+1, pady = 0,padx=8,sticky = 'ew',rowspan=reg_size,columnspan=right)
-lbdh.grid(row = int(reg_size *3+side_ld_size+1), column = left+1, pady = 0,padx=8, sticky = 'ns',rowspan=side_ld_size-reg_size,columnspan=right)#,ipady = 30
-dhsb1.grid(row = int(reg_size *3+side_ld_size+1), column = right+left, sticky = 'ns',rowspan=side_ld_size)
-dhsb2.grid(row = int(reg_size *2+side_ld_size*2+1),column = left+1,sticky = 'ew',rowspan=reg_size,columnspan=right,padx=pad)
-Checkbutton(text = 'DH with "G F Marking"', variable = naming).grid(row = int(reg_size *3+side_ld_size*2+1), column = left+1, pady = 0,padx=8,sticky = 'w',rowspan=reg_size,columnspan=right)
-
-
 
 # the file type that the load methods accept
 fTyp=[('text file','*.txt')]
@@ -181,8 +141,6 @@ def update_lb(text):
     lb.config(state=NORMAL)
     lb.delete(1.0,END)
     lb.insert('end',text)
-    #for s in text.split("\n"):
-    #    lb.insert('end', s)
     lb.config(state=DISABLED)
 
 def plot_fd():
@@ -590,11 +548,6 @@ lbdh.bind('<Double-1>', remove_dh)
 lbs.bind('<Button-3>', update_display_s)
 lbdh.bind('<Button-3>', update_display_dh)
 
-# control the bihavior of resising window, see what happens when you change the size
-#root.grid_columnconfigure(0,weight=1)
-#root.grid_rowconfigure(1,weight=1)
-#root.grid_rowconfigure(2,weight=1)
-
 # adding menu
 m = Menu(root)
 root.configure(menu = m)
@@ -737,31 +690,73 @@ def concordance():
     search_word = search_buffer.get().lower()
     if search_word == "":
         return
+    lb.config(state=NORMAL)
+    lb.delete(1.0,END)
     if nn.get():
         text_dict =  fileToDict(text)
         for chap in text_dict:
             for verse in text_dict[chap]:
                 word = text_dict[chap][verse]
-                if search_word in word.lower():
-                    word = re.sub(search_word,'"'+search_word+'"',word)
-                    display_result += str(chap)+":"+str(verse)+word
+                if re.match("[^\n]*"+search_word+"[^\n]*",word.lower()):
+                    three = re.match("([^\n]*)("+search_word+")([^\n]*)",word.lower()).groups()
+                    firsthalf = three[0]
+                    match = three[1]
+                    rest =three[2]
+                    lb.insert('end',str(chap)+":"+str(verse)+firsthalf)
+                    lb.insert('end',match,'search')
+                    lb.insert('end',rest+"\n\n")
     else:
         wl = word_tokenize(text)
         start = thismuch
         end = len(wl)-thismuch
         for index in range(0,start):
-            if wl[index].lower() == search_word:
-                display_result += " ".join(wl[0:index+1])
-                display_result +="\n\n"
+            if re.match("[^\n]*"+search_word+"[^\n]*", wl[index].lower()):
+                lb.insert('end',wl[0])
+                for i in range(1,index):
+                    if wl[i] in string.punctuation:
+                        lb.insert('end',wl[i])
+                    else:
+                        lb.insert('end'," " + wl[i])
+                lb.insert(re.match('end',"([^\n]*)("+search_word+")([^\n]*)",wl[index].lower()).groups()[1],'search')
+                lb.insert('end',"\n\n")
         for  index in range(start,end):
-            if wl[index].lower() == search_word:
-                display_result += " ".join(wl[index-thismuch:index+thismuch+1])
-                display_result +="\n\n"
+            if  re.match("[^\n]*"+search_word+"[^\n]*", wl[index].lower()):
+                lb.insert('end',wl[index-thismuch])
+                for i in range(index-thismuch+1,index):
+                    if wl[i] in string.punctuation:
+                        lb.insert('end',wl[i])
+                    else:
+                        lb.insert('end', " "+wl[i])
+                lb.insert('end',re.match("([^\n]*)("+search_word+")([^\n]*)",wl[index].lower()).groups()[1],'search')
+                for i in range(index+1,index+thismuch+1):
+                    if wl[i] in string.punctuation:
+                        lb.insert('end',wl[i])
+                    else:
+                        lb.insert('end', " "+wl[i])
+                lb.insert('end',"\n\n")
         for  index in range(end,end+thismuch):
-            if wl[index].lower() == search_word:
-                display_result += " ".join(wl[index:])
-                display_result +="\n\n"
-    update_lb(display_result)
+            if  re.match("[^\n]*"+search_word+"[^\n]*", wl[index].lower()):
+                lb.insert('end',re.match("([^\n]*)("+search_word+")([^\n]*)",wl[index].lower()).groups()[1],'search')
+                lb.insert('end',wl[end])
+                for i in range(end+1,end+index):
+                    if wl[i] in string.punctuation:
+                        lb.insert('end',wl[i])
+                    else:
+                        lb.insert('end', " "+wl[i])
+                lb.insert('end',"\n\n")
+                #display_result += " ".join(wl[index:])
+                #display_result +="\n\n"
+    #lb.config(state=NORMAL)
+    #lb.tag_configure("search", background = '#F0B325')
+    #lb.delete(1.0,END)
+    #for word in result_list:
+     #   if search_word in word:
+      #      lb.insert('end',word,)
+       # else:
+       #     lb.insert('end',word)
+       # lb.insert('end',word,'search')
+    lb.config(state=DISABLED)
+    #update_lb(display_result)
     
 menu_concordance  = Menu(m)
 m.add_cascade(label='Concordance',menu=menu_concordance,underline=0)
@@ -780,27 +775,37 @@ def marking_help():
               "Bible verses corresponding to the user-selected source text."
               )
 
-def cfd_help():
-    update_lb("CFD")
-
 def fd_help():
-    update_lb("CFD")
+    update_lb("\nFdist, or Frequency Distribution, is a method that tallies the occurrence of every word in a given corpus and returns the ones that occur most frequently in graphical form. The default value is 20, which means the 20 most common words will be listed in the results, though this can be changed in the input field."+
+              "\n\nTo use it, select a book of the Bible from the Sources box and choose one of the three plotting options:\n\n"+
+              "1. Plot by Value: \n"+
+              "2. Plot by Percentage:\n"
+              "3. Plot Hapax: Short for “hapax legomenon,” a term in corpus linguistics for any word in a corpus that occurs only once. Choosing this method will not return a graph but a list of all hapax legomena for a given book of the Bible in the primary display box.")
+
+def cfd_help():
+    update_lb("CFD, or Conditional Frequency Distribution, operates much like Fdist, with the\n\n"+
+              "Plot by Value: raw hits\n\n"+
+              "Plot by Percentage: relative to the size of the text (100%), the word tallies relative to the size of the text.")
+
+
+def concordance_help():
+    update_lb("\n"+"The concordance function takes a word from the input field and, if the word exists in the selected corpus, each occurrence of the word is displayed along with its surrounding context. As well as its verse and chapter location.")
 
 menu_help = Menu(m)
 m.add_cascade(label='Help',menu=menu_help,underline=0)
 menu_help.add_command(label=DH_FILE_CALLED,under=0,command=marking_help)
+menu_help.add_command(label='FD',under=0,command=fd_help)
 menu_help.add_command(label='CFD',under=0,command=cfd_help)
+menu_help.add_command(label='Concordance',under=0,command=concordance_help)
 
 # if there is a setting file load that to set up the settings
-nn.set(True)
-naming.set(False)
 if os.path.exists(directory + "setting.txt"):
     set_text = open(directory + "setting.txt").read()
-    if re.match('[^/]*Sources with "n:n"[^/]*=[^/]*False',set_text):
+    if re.match('[^/]*Sources with "n:n"[^\n]*=[^\n]*False',set_text):
         nn.set(False)
-    if re.match('[^/]*Marking with "G F Marking"[^/]*=[^/]*True',set_text):
+    if re.match('[^/]*Marking with "G F Marking"[^\n]*=[^\n]*True',set_text):
         naming.set(True)
-    if not re.match('[^/]*Load Marking automatically[^/]*=[^/]*False',set_text):
+    if not re.match('[^/]*Load Marking automatically[^\n]*=[^\n]*False',set_text):
         # load text files from "dh" folder if it exits
         if os.path.isdir(directory+ "dh"):
             file_directories = []
@@ -809,7 +814,7 @@ if os.path.exists(directory + "setting.txt"):
                     file_directories.append(directory + "dh/" + filename)        
         if len(file_directories) > 0:
             open_files(file_directories, dhdict, lbdh)
-    if not re.match('[^/]*Load Sources automatically[^/]*=[^/]*False',set_text):
+    if not re.match('[^/]*Load Sources automatically[^\n]*=[^\n]*False',set_text):
         # load text files from "text" folder if it exits
         if os.path.isdir(directory+ "text"):
             file_directories = []
@@ -818,23 +823,74 @@ if os.path.exists(directory + "setting.txt"):
                     file_directories.append(directory + "text/" + filename)        
             if len(file_directories) > 0:
                 open_files(file_directories, sourcedict, lbs)
-    if re.match('[^/]*default numbe[^\d]*(\d+)',set_text):
-        buffer.set(int(re.search('[^/]*default numbe[^\d]*(\d+)',set_text).group(1)))
-    if re.match('[^/]*default search word[^/]*=[^/]*"([^/]+)"',set_text):
-        search_buffer .set(re.search('[^/]*default search word[^/]*=[^/]*"([^/]+)"',set_text).group(1))
+    if re.match('[^/]*default numbe[^\n]*(\d+)',set_text):
+        buffer.set(int(re.search('[^/]*default numbe[^\d\n]*(\d+)',set_text).group(1)))
+    if re.match('[^/]*default search word[^\n]*=[^\n]*"([^\n]+)"',set_text):
+        search_buffer .set(re.search('[^/]*default search word[^\n]*=[^\n]*"([^\n]+)"',set_text).group(1))
 
 # set the welcome screen
-update_lb(" ____   ___    _"+"\n"+
-          "| __ ) / _ \  / \ "  +"\n"+
-          "|  _ \| | | |/ _ \  "+"\n"+
-          "| |_) | |_| / ___ \ "+"\n"+"|____/ \___/_/   \_\ "
-          +"\nWelcome to BOA, the Biblical Origins Analyzer!\n"+
-          "This program gathers together several text analysis tools "+
-          "from the Python-based NLTK (Natural Language Toolkit)\n"+
-          "suite into one easy-to-use space for Old Testament scholars.\n"+
-          "Written in Python 3\n"+
-          "Created by Akio Kist-Okazaki and Tristan Engel"
+lb.tag_configure("search", background = '#F0B325')
+lb.config(state=NORMAL)
+lb.delete(1.0,END)
+lb.insert('end',"   ____   ___    _     ",'search')
+lb.insert('end',"\n")
+lb.insert('end', "  | __ ) / _ \  / \    " ,'search')
+lb.insert('end',"\n")
+lb.insert('end',"  |  _ \| | | |/ _ \   ",'search')
+lb.insert('end',"\n")
+lb.insert('end',"  | |_) | |_| / ___ \  ",'search')
+lb.insert('end',"\n")
+lb.insert('end',"  |____/ \___/_/   \_\ ",'search')
+lb.insert(
+          'end',
+          "\n\nWelcome to BOA, the Biblical Origins Analyzer! This program gathers together several text analysis tools from the Python-based NLTK (Natural Language Toolkit) suite into one easy-to-use space for Torah scholars doing compositional analysis.\n\n"+
+          "BOA was constructed around the documentary hypothesis (DH) of Pentateuchal biblical scholarship, that \n"+
+          "the Pentateuch as we have it today originated as four separate textual sources, the Yahwist (J), Elohist (E), Deuteronomic (D) and Priestly (P) sources, which were combined\n\n"+
+          "That the Pentateuch was originally four separate sources that were combined over time \n"+
+          "For example, Genesis 1:1-2:3, which constitutes the seven day Creation account, is considered syntactically different enough from the rest of Genesis 2 that documentary hypothesis scholars have judged the two sections to have been composed at different times by different writers."
+          "\n\nCreated by Akio Kist-Okazaki and Tristan Engel."
           )
-          
+lb.config(state=DISABLED)
+# set the size of things
+lb_size = 180
+# 40% each, total 80 %
+side_ld_size =  int(lb_size/2.5)
+# 5% each 20% total
+reg_size = int(lb_size/20)
+lb_size = lb_size-reg_size
+left = 30
+right = 10
+pad = 3
+search_width = int(left/1.5)
+spin_width = int(left/6)
+
+# create button for search
+search_button = Button(root, text = 'Search', command = concordance)
+# set the grid
+# left side top to down
+lb.grid(row = 0, column = 0, sticky = 'nsew',  padx = pad, pady = pad,rowspan=lb_size,columnspan= left) # ipadx = 200, ipady = 140,
+sb1.grid(row = 0, column = left, sticky = 'ns',rowspan=lb_size)
+e.grid(row = lb_size+1, column = 0,columnspan = search_width, sticky = 'ew',  padx = 10, pady = 7,rowspan=reg_size)
+search_button.grid(row = lb_size+1, column = search_width,columnspan = spin_width, sticky = 'ew',  padx = 2, pady = 2,rowspan=reg_size)
+n_spin.grid(row = lb_size+1, column = search_width+spin_width+1,columnspan = spin_width, sticky = 'ew',  padx = 2, pady = 2,rowspan=reg_size)
+
+#right side top to down
+lbs_label.grid(row = 0, column = left+1, pady = pad,padx=pad,sticky = 'ew',rowspan=reg_size,columnspan=right)
+lbs.grid(row =  reg_size+1,column = left+1, pady = 0,padx=0,sticky = 'ns',rowspan=side_ld_size-reg_size,columnspan=right)#ipady = 40
+ssb1.grid(row =  reg_size+1,column = right +left, sticky = 'ns',columnspan=right,rowspan=side_ld_size-reg_size)
+ssb2.grid(row =  side_ld_size+1, column = left+1, sticky = 'ew',rowspan=reg_size,padx=pad,columnspan=right)
+Checkbutton(text = 'Source with "n:n"', variable = nn).grid(row = side_ld_size+reg_size+1, column = left+1, pady = 0,padx=8,sticky = 'w',rowspan=reg_size,columnspan=right)
+lbdh_label.grid(row = int(reg_size *2+side_ld_size+1), column = left+1, pady = 0,padx=8,sticky = 'ew',rowspan=reg_size,columnspan=right)
+lbdh.grid(row = int(reg_size *3+side_ld_size+1), column = left+1, pady = 0,padx=8, sticky = 'ns',rowspan=side_ld_size-reg_size,columnspan=right)#,ipady = 30
+dhsb1.grid(row = int(reg_size *3+side_ld_size+1), column = right+left, sticky = 'ns',rowspan=side_ld_size)
+dhsb2.grid(row = int(reg_size *2+side_ld_size*2+1),column = left+1,sticky = 'ew',rowspan=reg_size,columnspan=right,padx=pad)
+Checkbutton(text = 'DH with "G F Marking"', variable = naming).grid(row = int(reg_size *3+side_ld_size*2+1), column = left+1, pady = 0,padx=8,sticky = 'w',rowspan=reg_size,columnspan=right)
+
+# control the bihavior of resising window, see what happens when you change the size
+for i in range(0, lb_size):
+    root.grid_rowconfigure(i,weight=1)
+for i in range(0, left+right):
+    root.grid_columnconfigure(i,weight=1)
+
 # this starts the app
 root.mainloop()
